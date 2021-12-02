745589e9f7490b852666baee99fdd8d28958a85e
gxx-github <ahylddsdz@163.com>
2021-12-02 11:21:01 +0800

    初始化搭建

---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+import { history } from "umi"
+// 导航守卫
+const whiteArr = ["/login", "403", "404", "/chat"]
+export function onRouteChange({ matchedRoutes, location }: any) {
+  if (matchedRoutes.length) {
+    document.title = matchedRoutes[matchedRoutes.length - 1].route.title || '';
+  }
+  // // 首页重定向到/postskill
+  if (location.pathname === "/") {
+    history.replace("/login")
+  }
+}
